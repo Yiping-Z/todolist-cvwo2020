abbfbdb62b7de2b79ab32897cb22b1d70f083b57
ZhangYiping126 <e0357438@u.nus.edu>
2020-01-06 21:52:01 +0800

    heroku links

--- app/javascript/components/Signup.js
+++ app/javascript/components/Signup.js
@@ -27,7 +27,7 @@ handleSubmit = (event) => {
       password: password,
       password_confirmation: password_confirmation
     }
-axios.post('http://localhost:3000/users', {user}, {withCredentials: true})
+axios.post('https://todolist-react-cvwo.herokuapp.com/users', {user}, {withCredentials: true})
     .then(response => {
       if (response.data.status === 'created') {
         this.props.handleLogin(response.data)
