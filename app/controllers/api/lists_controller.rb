75369cb88c3a22ccd943d38edfc10ac9cf942b0b
ZhangYiping126 <e0357438@u.nus.edu>
2020-01-06 18:31:45 +0800

    list save

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -12,6 +12,7 @@ class Api::ListsController < ApplicationController
     def create
       @list = List.create(list_params)
       @list.user = current_user
+      @list.save
       respond_with :api, @list
     end
   
